15da0b5eba48ab84d4f4669198a8f0247c43f6f9
Sean Lee <seule@amazon.com>
2021-01-19 15:31:30 +0900

    initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+ 
+app.listen(port)
+
+console.log('Server running at http://127.0.0.1:' + port + '/');
